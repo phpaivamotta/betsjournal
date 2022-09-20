b628194da9be88667559d4c20b826af847780da6
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-09-19 23:48:46 -0300

    tried to put back-ground image and text on welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,17 @@
 <x-app-layout>
-    
+    <div class="mt-[-2px] w-full h-64 bg-no-repeat bg-cover bg-left bg-fixed"
+        style="background-image: url('https://cdn.pixabay.com/photo/2016/11/29/07/10/hand-1868015_1280.jpg');"
+    >
+
+        <div class="h-full">
+            <p class="text-white text-lg text-center">
+                Keep track of your bets.
+            </p>
+        </div>
+
+    </div>
+
+    <div class="h-64">
+
+    </div>
 </x-app-layout>
