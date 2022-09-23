0f917b1772d3fc37f5003f166931b5614059c794
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-09-23 20:44:43 -0300

    created database schema and migrated it

--- app/Models/Bet.php
+++ app/Models/Bet.php
@@ -9,5 +9,5 @@ class Bet extends Model
 {
     use HasFactory;
 
-    
+    protected $guarded = [];
 }
--- database/migrations/2022_09_18_191216_create_bets_table.php
+++ database/migrations/2022_09_18_191216_create_bets_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('bets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('sport')->nullable();
+            $table->string('match');
+            $table->date('match_date')->nullable();
+            $table->time('match_time')->nullable();
+            $table->string('bookie')->nullable();
+            $table->string('bet_type')->nullable();
+            $table->string('bet_description')->nullable();
+            $table->string('bet_pick')->nullable();
+            $table->decimal('bet_size');
+            $table->float('odds');
+            $table->boolean('result')->nullable();
             $table->timestamps();
         });
     }
