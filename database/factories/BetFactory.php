f5a7bd06fdcaa89925249470eac2057f2720ced9
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-09-24 16:55:56 -0300

    set up migation and factory for Bet model. Also defined eloquent relationship between Bet and User models

--- database/factories/BetFactory.php
+++ database/factories/BetFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bet>
@@ -17,7 +18,18 @@ class BetFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'sport' => fake()->word(),
+            'match' => fake()->sentence(),
+            'match_date' => fake()->date(),
+            'match_time' => fake()->time(),
+            'bookie' => fake()->word(),
+            'bet_type' => fake()->word(),
+            'bet_description' => fake()->sentence(),
+            'bet_pick' => fake()->word(),
+            'bet_size' => fake()->randomFloat(2, 100, 300),
+            'odds' => fake()->randomFloat(2, 2, 10),
+            'result' => fake()->boolean(),
         ];
     }
 }
