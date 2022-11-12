8b3d31ff9ab0d7fd601d77d90132e0bc2c4f2013
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-11-11 22:15:59 -0300

    substituted email logo image from svg to png

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" class="w-5" style="display: inline-block;">
 
-<img src="{{ asset('img/betsjournal-email-logo.svg') }}" width="100" alt="Betsjournal Logo">
+<img src="{{ asset('img/betsjournal-email-logo.png') }}" width="100" alt="Betsjournal Logo">
 
 </a>
 </td>   
