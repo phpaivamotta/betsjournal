1267f39eede21794c6cfe9f9e48bb23e89f55ab3
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2023-01-30 12:30:16 -0300

    reverting test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,8 +19,7 @@ class Kernel extends ConsoleKernel
 
         // send value bets emails
         $schedule->command('valuebetsemails:send')
-            // ->dailyAt('10:30'); // 10:30 = 7:30 in America/Sao_Paulo time 
-            ->everyMinute(); // 10:30 = 7:30 in America/Sao_Paulo time 
+            ->dailyAt('10:30'); // 10:30 = 7:30 in America/Sao_Paulo time 
     }
 
     /**
